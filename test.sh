8c3487b7844fb8937f75f5dbae8b113677588659
rjshrjndrn <rjshrjndrn@gmail.com>
2018-05-30 11:37:55 +0530

    Update var

--- test.sh
+++ test.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 # added sample comment
 
-version=$(cat version.env)
-docker run -d -p 8000:8000 -d rjsjrjndrn/dev:${version}_${TRAVIS_COMMIT}
+docker run -d -p 8000:8000 -d ${IMAGE_NAME}
 sleep 2s
 curl -sS --fail localhost:8000 || exit 1
