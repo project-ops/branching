1a5b1822588e2759c76864d666603dab87180e63
rjshrjndrn <rjshrjndrn@gmail.com>
2018-05-30 12:14:50 +0530

    fix

--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # added sample comment
 
-verson=$(cat verson.env)
+verson=$(cat version.env)
 docker run -d -p 8000:8000 -d rjsjrjndrn/dev:${version}_latest
 sleep 2s
 curl -sS --fail localhost:8000 || exit 1
