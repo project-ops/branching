05d1abcb244a7fe7157dd5916c1fc0cb4a8b84bb
rjshrjndrn <rjshrjndrn@gmail.com>
2018-05-30 12:34:37 +0530

    Updated docker push

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+
 echo "$1" | docker login -u rjsjrjndrn --password-stdin
-echo "image name is : $2"
-docker push rjsjrjndrn/$2
+
+images=$(docker images | grep rjsjrjndrn)
+
+for image in $images;do
+  docker push $image
+done
