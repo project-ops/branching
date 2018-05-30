2e305a626238a4ff79ffff4d037190d22d2cd0a1
rjshrjndrn <rjshrjndrn@gmail.com>
2018-05-30 12:38:29 +0530

    Updated version passing

--- build.sh
+++ build.sh
@@ -5,6 +5,6 @@
 # release_branch ==> images to qa
 # relase_branch merged to master => gold image to docker public repo (image name: dev/major-version_latest,version)
 
-export version=$(cat version.env)
+version=$(cat version.env)
 docker build -t rjsjrjndrn/dev:${version}_latest .
 docker build -t rjsjrjndrn/dev:${version}_${TRAVIS_COMMIT} .
