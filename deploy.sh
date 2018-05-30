3d619045c2f9249faf9d80c6b6f13cd654ae19d8
rjshrjndrn <rjshrjndrn@gmail.com>
2018-05-30 15:08:56 +0530

    Update

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 
 echo "$1" | docker login -u rjsjrjndrn --password-stdin
 
-images=$(docker images | grep rjsjrjndrn)
+images=$(docker images | grep rjsj | awk '{print $1}')
 
 for image in $images;do
   echo image is $image
